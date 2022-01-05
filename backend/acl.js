508eee5d92b6befb8fb4a6b2c92f489eb13ba5a2
TiredBirb <72065138+anna636@users.noreply.github.com>
2022-01-05 21:35:49 +0100

    Deleted comments

--- backend/acl.js
+++ backend/acl.js
@@ -14,7 +14,7 @@ module.exports = function (req, res, next) {
     roles = ["anonymous"];
   }
 
-  // let roles = req.session?.user?.roles || ["anonymous"];
+
   roles.push("*");
 
   console.log({
