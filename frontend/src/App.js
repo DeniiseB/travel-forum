df266a583eec4c76024723c5abdd50f973b7a833
nicolebradleey <nicole_bradley94@hotmail.com>
2022-01-07 12:32:22 +0100

    Created Inside Category page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import InsideCategory from './pages/InsideCategory';
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <InsideCategory/>
     </div>
   );
 }
---
+++ frontend/src/pages/InsideCategory.jsx
@@ -0,0 +1,9 @@
+function InsideCategory() {
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+
+export default InsideCategory;
