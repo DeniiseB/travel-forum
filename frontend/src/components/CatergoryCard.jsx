09f356f712917acf97d15b5e0f54501693b997f9
JacobLexhag <72065984+JacobLexhag@users.noreply.github.com>
2022-01-07 13:11:08 +0100

    added test data for categoryCard on HomePage

---
+++ frontend/src/components/CatergoryCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardTitle, CardText, Row, Col } from 'reactstrap';
+
+function CategoryCard(props) {
+  
+  return (
+    <Col sm="6">
+      <Card body style={styles.card}>
+        <div style={styles.cardCategory}>
+          <CardTitle >{props.props.category}</CardTitle>
+        </div>
+        <div style={styles.cardOwner}>
+          <CardTitle>{props.props.groupAmount}</CardTitle>
+        </div>
+      </Card>
+    </Col>
+  );
+}
+export default CategoryCard;
+
+const styles = {
+  
+  card: {
+  },
+  cardCategory: {
+    display: "flex",
+    flexDirection: "row",
+    position: "absolute",
+  },
+  cardOwner: {
+    marginLeft: "250px",
+  },
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,35 @@
+import React from 'react';
+import { ListGroup, ListGroupItem, } from 'reactstrap';
+import CategoryCard from '../components/CatergoryCard';
 
 function Home() {
+  const data = [{ "category": "USA", "groupAmount": 10 }, { "category": "Sweden", "groupAmount": 23 }];
+
   return (
     <div className="Home">
-      <h1>Categorys</h1>
+      <h2 style={styles.catergoryTitle}>Categorys</h2>
+      <h5 style={styles.groupAmountTitle}>Group Amount</h5>
+      <div>
+          {data.map((item, index) => (
+            <CategoryCard props={item} key={index} />
+          ))}
+      </div>
     </div>
   );
 }
 
 
 export default Home;
+
+const styles = {
+  catergoryTitle: {
+    marginTop: "2vh",
+    display: "flex",
+    flexDirection: "row",
+    position: "absolute",
+  },
+  groupAmountTitle: {
+    marginTop: "2vh",
+    marginLeft: "250px",
+  }
+}
