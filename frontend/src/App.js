cd8cbbadfbc5419f726c8cfad8e2e3ca4594c041
nicolebradleey <nicole_bradley94@hotmail.com>
2022-01-07 17:52:22 +0100

    Added route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-
 import './App.css';
 import InsideCategory from './pages/InsideCategory';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <InsideCategory/>
+      <Router>
+        <Routes>
+          <Route exact path="/inside-category" component={InsideCategory} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
