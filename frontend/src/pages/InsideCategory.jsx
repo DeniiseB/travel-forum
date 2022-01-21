b76584b76e37ae49ca769d6634c24fd3ffeabecd
Denise <72066356+DeniiseB@users.noreply.github.com>
2022-01-21 15:26:02 +0100

    inside category styled

--- frontend/src/pages/InsideCategory.jsx
+++ frontend/src/pages/InsideCategory.jsx
@@ -5,7 +5,6 @@ import { useParams } from "react-router-dom";
 import { useGroupContext } from "../contexts/GroupContext";
 import { Link } from "react-router-dom";
 
-
 function InsideCategory() {
   const { getCategoryById, getGroupIdsByCategoryId } =
     useContext(CategoryContext);
@@ -27,27 +26,29 @@ function InsideCategory() {
 
   async function fetchGroups() {
     const fetchedGroupIds = await getGroupIdsByCategoryId(id);
-    const groupsArray = []
+    const groupsArray = [];
     for (let idObject of fetchedGroupIds) {
-      let group = await fetchGroupById(idObject.groupId)
-      groupsArray.push(group)
+      let group = await fetchGroupById(idObject.groupId);
+      groupsArray.push(group);
     }
-    setGroups(groupsArray)
-   }
+    setGroups(groupsArray);
+  }
 
   return (
     <div className="App">
-      <p>
-        <Link style={styles.link} to="/create-group">
-          Create group
+      <div style={styles.createGroupContainer}>
+        <Link to="/create-group">
+          <div style={styles.createGroup}>Create group</div>
         </Link>
-      </p>
+      </div>
       {category && (
         <Table className="table table-hover" style={styles.categoriesTable}>
           <thead>
             <tr>
               <th scope="colSpan">
-                <h1> {category.name}</h1>
+                <div style={styles.categoryName}>
+                  <p> {category.name}</p>
+                </div>
               </th>
             </tr>
           </thead>
@@ -55,9 +56,11 @@ function InsideCategory() {
             <tbody>
               {groups.map((group) => (
                 <tr key={group.id}>
-                  <Link style={styles.link2} to={"/group/" + group.id}>
+                  <Link to={"/group/" + group.id}>
                     {" "}
-                    <th scope="row">{group.groupName}</th>
+                    <th scope="row">
+                      <div style={styles.groupName}>{group.groupName}</div>
+                    </th>
                   </Link>
                 </tr>
               ))}
@@ -79,19 +82,43 @@ export default InsideCategory;
 const styles = {
   categoriesTable: {
     width: "15rem",
-    border: "solid lightGrey 1px",
     borderRadius: "4px",
     marginLeft: "auto",
     marginRight: "auto",
-    marginTop: "5rem",
+    marginTop: "3rem",
+    backgroundColor: "#424242c9",
+    color: "white",
   },
-  link: {
-    textDecoration: "none",
-    color: "black",
-    marginLeft: "15rem",
+  categoryName: {
+    marginTop: "0.8rem",
+    fontFamily: "Montserrat, sans-serif",
+    fontStyle: "italic",
+    fontWeight: "600",
+    fontSize: "1.2em",
   },
-  link2: {
+  groupName: {
+    margin: "0.5rem",
+    fontFamily: "Montserrat, sans-serif",
+    fontStyle: "italic",
+    fontWeight: "500",
+    fontSize: "1em",
+    color: "white",
+  },
+  createGroupContainer: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "flex-end",
+  },
+  createGroup: {
+    border: "solid 1px #424242",
+    borderRadius: "4px",
     textDecoration: "none",
-    color: "black",
+    color: "#424242",
+    fontFamily: "Montserrat, sans-serif",
+    fontStyle: "italic",
+    fontWeight: "500",
+    fontSize: "1em",
+    padding: "0.4rem",
+    margin: "1rem",
   },
 };
